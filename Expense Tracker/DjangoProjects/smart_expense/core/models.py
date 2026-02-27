ef343a2f09aa87e8ec6a8dad0d3695d198b85b58
Nithyashreeja <nithyashreejagopi@gmail.com>
2026-02-27 20:22:19 +0530

    Added Expense model with location field

--- Expense Tracker/DjangoProjects/smart_expense/core/models.py
+++ Expense Tracker/DjangoProjects/smart_expense/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db import models
 
+class Expense(models.Model):
+    amount = models.FloatField()
+    description = models.TextField()
+    date = models.DateField()
+    location = models.CharField(max_length=200, null=True, blank=True)
 # Create your models here.
